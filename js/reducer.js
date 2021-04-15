cc723787ac59a111b54d970506bbfc751360fd1d
chrislemus <icrislemus@gmail.com>
2021-04-15 08:23:14 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,25 @@
 // add code snippets from README
+let state;
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({ type: '@@INIT' });
+
+const button = document.querySelector('#button');
+button.addEventListener('click', () => dispatch({ type: 'INCREMENT' }));
